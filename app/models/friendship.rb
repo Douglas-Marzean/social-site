79886c39a8cc65b3a9cee148d09a8e55072e0605
AdStyle Marketing <doug@adstylemarketing.com>
2016-04-01 09:08:58 -0500

    Added friendship types

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,8 @@
 class Friendship < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :friend, class_name: "User"
+	belongs_to :user
+	belongs_to :friend, class_name: "User"
 
-  def accept_friendship
+	def accept_friendship
 		self.update_attributes(state: "active", friended_at: Time.now)
 	end
 
